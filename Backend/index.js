5ecfcf76a2d8cb9d269abc78b785c552dd656444
Rafael Paraiso <rafaelparaiso.bsu@gmail.com>
2025-01-08 05:51:44 +0400

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 4000;
 
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use('/api/user', userRoutes)
 
